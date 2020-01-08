cbe910903a4f4d7f6b8ec9a567208d126e5f0b37
OmarHamdySh <omar.h.shafeek@gmail.com>
2020-01-08 20:51:57 +0200

    Omar - Adding Starter Scripts

---
+++ Assets/Scripts/Animation FSM/WorkerAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkerAnimation : MonoBehaviour
+{
+    public TheAnimationState characterAnimation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
