8280877df6f3e6403935369d5ae78efbe1ba51c3
Osama Elsharkawy <Osama-nagy@hotmail.com>
2020-01-09 07:33:20 +0200

    Adding Event System
    Adding "Event System" controlled by Scriptable Objects

---
+++ Assets/Osama/Scripts/Event System/Editor/GameEventEditor_Osama.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// This scrips creates editor button -Raise Event- to raise any event from editor
+/// </summary>
+
+[CustomEditor(typeof(GameEvent_Osama))]
+public class GameEventEditor_Osama : Editor
+{
+    public override void OnInspectorGUI()//  Draw Button inside unity editor
+    {
+        base.OnInspectorGUI();
+        GUI.enabled = Application.isPlaying;
+        GameEvent_Osama e = target as GameEvent_Osama;
+        if (GUILayout.Button("Raise Event"))
+        {
+            e.Raise();
+        }
+    }
+}
