0d8c182a6d5421ac9109d5d9f5b931b2981792e2
Omar <omar.h.shafeek@gmail.com>
2020-01-13 23:39:11 +0200

    Omar - Adding Models & Path Component

---
+++ Assets/_AppAssets/Scripts/PathManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.DemiLib;
+using DG.Tweening;
+
+public class PathManager : MonoBehaviour
+{
+    DOTweenPath doTweenPath;
+    int currentIndex;
+    int initialIndex;
+    // Start is called before the first frame update'
+    private void Awake()
+    {
+        doTweenPath = GetComponentInParent<DOTweenPath>();
+        Init();
+    }
+
+
+    public void Init() {
+        PathManagerManager  pmm = GetComponentInParent<PathManagerManager>();
+        initialIndex = pmm.pathManagers.IndexOf(this);
+        transform.position = doTweenPath.wps[initialIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //if (transform.position == doTweenPath.wps[currentIndex])
+            //{
+            //    currentIndex++;
+            //}
+
+
+            transform.DOMove(doTweenPath.wps[Mathf.Clamp(initialIndex++, 0, doTweenPath.wps.Count )],0.5f);
+
+            if (initialIndex == doTweenPath.wps.Count)
+            {
+                initialIndex = 0;
+            }
+        }
+    }
+}
---
+++ Assets/_AppAssets/Scripts/PathManagerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathManagerManager : MonoBehaviour
+{
+    public List<PathManager> pathManagers;
+
+
+    public void Start()
+    {
+        if (pathManagers.Count>0)
+        {
+            foreach (var item in pathManagers)
+            {
+                //item.Init(this);
+            }
+        }
+    }
+}
