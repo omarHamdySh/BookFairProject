0b813226e7767e58db9963f31a3187e91154c408
Bendary <ahmed_fthe2040@yahoo.com>
2020-01-15 12:59:51 +0200

    Bendary - modify Camera path move

--- Assets/_AppAssets/Scripts/General/TestCamerPath.cs
+++ Assets/_AppAssets/Scripts/General/TestCamerPath.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class TestCamerPath : MonoBehaviour
 {
-    [SerializeField] private float speedToMove;
+    [SerializeField] private float toFloorSpeed, toBockcaseSpeed, toBookcaseShelfSpeed, toBookcaseBook;
     [SerializeField] private Transform cameraPos;
     [SerializeField] private Transform[] rootWayPoints;
 
-    [SerializeField] private bool isRoot = true, isMoving;
-    [SerializeField] private int row = 0, column = 0;
+    private bool isRoot = true, isMoving;
+    private int row = 0, column = 0, prevRow = 0, prevColumn = 0;
+    private float currSpeed = 0;
 
     private void Update()
+    {
+        DecideMovingAccordingToInput();
+        MoveCamera();
+    }
+
+    private void DecideMovingAccordingToInput()
     {
         if (!isMoving)
         {
@@ -21,11 +28,13 @@ public class TestCamerPath : MonoBehaviour
                 {
                     row++;
                     isMoving = true;
+                    DecideSpeed();
                 }
                 else if (Input.GetKeyDown(KeyCode.DownArrow) && row > 0)
                 {
                     row--;
                     isMoving = true;
+                    DecideSpeed();
                 }
             }
             else if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Escape))
@@ -35,11 +44,13 @@ public class TestCamerPath : MonoBehaviour
                     column++;
                     isMoving = true;
                     isRoot = false;
+                    DecideSpeed();
                 }
                 else if (Input.GetKeyDown(KeyCode.Escape) && column > 0)
                 {
                     column--;
                     isMoving = true;
+                    DecideSpeed();
                     if (column == 0)
                     {
                         isRoot = true;
@@ -47,11 +58,38 @@ public class TestCamerPath : MonoBehaviour
                 }
             }
         }
-        else if (isMoving)
+    }
+
+    private void DecideSpeed()
+    {
+        if (row != prevRow)
+        {// Move from floor to floor
+            currSpeed = toFloorSpeed;
+        }
+        else if ((prevColumn == 0 && column == 1) || (prevColumn == 1 && column == 0))
+        {// Move from floor to bookcase or vice versa
+            currSpeed = toBockcaseSpeed;
+        }
+        else if ((prevColumn == 1 && column == 2) || (prevColumn == 2 && column == 1))
+        {// Move from bookcase to bookcase shelf or vice versa
+            currSpeed = toBookcaseShelfSpeed;
+        }
+        else if ((prevColumn == 2 && column == 3) || (prevColumn == 3 && column == 2))
+        {// Move from bookcase shelf to bookcase book or vice versa
+            currSpeed = toBookcaseBook;
+        }
+    }
+
+    private void MoveCamera()
+    {
+        if (isMoving)
         {
-            cameraPos.position = Vector3.Lerp(cameraPos.position, rootWayPoints[row].GetChild(column).position, speedToMove * Time.deltaTime);
+            cameraPos.position = Vector3.Lerp(cameraPos.position, rootWayPoints[row].GetChild(column).position,
+                currSpeed * Time.deltaTime);
             if (cameraPos.position == rootWayPoints[row].GetChild(column).position)
             {
+                prevColumn = column;
+                prevRow = row;
                 isMoving = false;
             }
         }
