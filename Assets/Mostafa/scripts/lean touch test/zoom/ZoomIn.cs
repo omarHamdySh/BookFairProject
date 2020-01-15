d51717b729e6f4f54608632f01e6b8728c32d215
sirius707 <steave_mudblood@yahoo.com>
2020-01-15 21:35:01 +0200

    test drag  tap mostafa

---
+++ Assets/Mostafa/scripts/lean touch test/zoom/ZoomIn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Touch;
+
+public class ZoomIn : MonoBehaviour
+{
+    public CameraHandler cameraHandler;
+    private LeanSelectable ls;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Lean.Touch.LeanTouch.OnFingerTap += zoomIn;
+        ls = GetComponent<LeanSelectable>();
+    }
+
+    void zoomIn(Lean.Touch.LeanFinger finger)
+    {
+        
+            if(ls.IsSelected)
+            {
+                    cameraHandler.select(transform);
+            }
+        
+    }
+
+    
+}
