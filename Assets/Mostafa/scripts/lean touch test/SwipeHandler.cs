8ea01363d7fcbdae1ca1ae3083a94a428a955e41
sirius707 <steave_mudblood@yahoo.com>
2020-01-16 19:14:24 +0200

    TestSwipe_Mostafa

---
+++ Assets/Mostafa/scripts/lean touch test/SwipeHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Touch;
+
+public class SwipeHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void swipedLeft()
+    {
+        Debug.Log("swipe has left");
+    }
+
+    public void swipedRight()
+    {
+        Debug.Log("swipe has right");
+    }
+}
