635dbac88d99f1c853d941d805a0646405a4c8fa
KLQUInfinity <31450453+KLQUInfinity@users.noreply.github.com>
2020-01-22 21:13:21 +0200

    bendary - make roll Animation in ui

---
+++ Assets/_AppAssets/Scripts/General/TestRollAnimation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRollAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator myAnim;
+
+    public void ToggleRollAnimation(bool check)
+    {
+        myAnim.SetBool("Open", check);
+    }
+}
