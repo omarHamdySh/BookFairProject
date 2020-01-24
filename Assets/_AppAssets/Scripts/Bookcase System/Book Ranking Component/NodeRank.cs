2c715c3a0dd6c128266d08054810193b46e8bff2
Omar <omar.h.shafeek@gmail.com>
2020-01-24 14:40:02 +0200

    Omar - Book Ranking Classes and Some Lighting Edits

---
+++ Assets/_AppAssets/Scripts/Bookcase System/Book Ranking Component/NodeRank.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeRank : MonoBehaviour
+{
+    public enum Rank { 
+    
+        r0,
+        r1,
+        r2,
+        r3
+    }
+
+    public Rank rank;
+    public int rankRotation;
+}
