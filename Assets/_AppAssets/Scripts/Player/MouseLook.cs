10663d40ebce66f8216547d427af38d32a297f68
Omar <omar.h.shafeek@gmail.com>
2020-02-05 16:59:21 +0200

    Omar - Refactoring the Scene
    Adding individual arches - adding new book model

--- Assets/_AppAssets/Scripts/Player/MouseLook.cs
+++ Assets/_AppAssets/Scripts/Player/MouseLook.cs
@@ -35,6 +35,7 @@ public class MouseLook : MonoBehaviour
     Quaternion originalRotation;
     public Rigidbody rigidbody;
 
+    public bool isFreeCamera;
     void Start()
     {
         // Make the rigid body not change rotation
@@ -45,31 +46,40 @@ public class MouseLook : MonoBehaviour
 
     void Update()
     {
-        if (axes == RotationAxes.MouseXAndY)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            // Read the mouse input axis
-            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationX = ClampAngle(rotationX, minimumX, maximumX);
-            rotationY = ClampAngle(rotationY, minimumY, maximumY);
-            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
-            Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
-            transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+            isFreeCamera = !isFreeCamera;
         }
-        else if (axes == RotationAxes.MouseX)
+        if (isFreeCamera)
         {
-            rotationX += Input.GetAxis("Mouse X") * sensitivityX;
-            rotationX = ClampAngle(rotationX, minimumX, maximumX);
-            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
-            transform.localRotation = originalRotation * xQuaternion;
+
+            if (axes == RotationAxes.MouseXAndY)
+            {
+                // Read the mouse input axis
+                rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+                rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+                rotationX = ClampAngle(rotationX, minimumX, maximumX);
+                rotationY = ClampAngle(rotationY, minimumY, maximumY);
+                Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+                Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
+                transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+            }
+            else if (axes == RotationAxes.MouseX)
+            {
+                rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+                rotationX = ClampAngle(rotationX, minimumX, maximumX);
+                Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+                transform.localRotation = originalRotation * xQuaternion;
+            }
+            else
+            {
+                rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+                rotationY = ClampAngle(rotationY, minimumY, maximumY);
+                Quaternion yQuaternion = Quaternion.AngleAxis(-rotationY, Vector3.right);
+                transform.localRotation = originalRotation * yQuaternion;
+            }
         }
-        else
-        {
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = ClampAngle(rotationY, minimumY, maximumY);
-            Quaternion yQuaternion = Quaternion.AngleAxis(-rotationY, Vector3.right);
-            transform.localRotation = originalRotation * yQuaternion;
-        }
+
     }
 
     public static float ClampAngle(float angle, float min, float max)
