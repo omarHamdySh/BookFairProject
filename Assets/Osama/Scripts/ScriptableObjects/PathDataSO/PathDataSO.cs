ab491b023b80283f9b2c89f712edaa7389e20530
Unknown <unknown@example.com>
2020-02-15 23:01:12 +0200

    Osama - First caching implementation

---
+++ Assets/Osama/Scripts/ScriptableObjects/PathDataSO/PathDataSO.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+[CreateAssetMenu(fileName = "PathDataSO", menuName = "DataManagers/PathData")]
+public class PathDataSO : ScriptableObject
+{
+    #region Private Variables
+
+    [Space, SerializeField, Header("Floors Scroll Speed"), Tooltip("Scrolling speed for Floors")]
+    private float floorScrollSpeed = 0;
+
+    [Space, SerializeField, Header("Bookcases Scroll Speed"), Tooltip("Scrolling speed for Bookcases")]
+    private float bookcaseScrollSpeed = 0;
+
+    [Space, SerializeField, Header("Shelves Scroll Speed"), Tooltip("Scrolling speed for Shelves")]
+    private float shelfScrollSpeed = 0;
+
+    [Space, SerializeField, Header("Books Scroll Speed"), Tooltip("Scrolling speed for Books")]
+    private float bookScrollSpeed = 0;
+
+    [Space, SerializeField, Header("Movement Ease")]
+    public Ease MovementEase;
+
+    
+
+    #endregion
+
+    #region Getters/Setter
+
+    public float FloorScrollSpeed { get => floorScrollSpeed; set => floorScrollSpeed = value; }
+    public float BookcaseScrollSpeed { get => bookcaseScrollSpeed; set => bookcaseScrollSpeed = value; }
+    public float BookScrollSpeed { get => bookScrollSpeed; set => bookScrollSpeed = value; }
+    public float ShelfScrollSpeed { get => shelfScrollSpeed; set => shelfScrollSpeed = value; }
+
+    #endregion
+}
