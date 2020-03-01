a0073f16ce96c657b3fc70dded34d09725f1dc29
KLQUInfinity <31450453+KLQUInfinity@users.noreply.github.com>
2020-03-01 23:54:48 +0200

    bendary - prebare for integration with mostafa in search

--- Assets/_AppAssets/Scripts/GUI/LevelUI.cs
+++ Assets/_AppAssets/Scripts/GUI/LevelUI.cs
@@ -21,6 +21,11 @@ public class LevelUI : UIHandller
     }
     #endregion
 
+    #region LoadingBar
+    [Header("LoadingBar")]
+    public GameObject loadingBar;
+    #endregion
+
     #region SwitchFromUI to Game Mode
     [HideInInspector] public bool isUIOpen = false;
 
@@ -29,4 +34,55 @@ public class LevelUI : UIHandller
         isUIOpen = check;
     }
     #endregion
+
+    #region Search
+    [Header("Search")]
+    [SerializeField] private Button prevSearchPageBtn, nextSearchPageBtn;
+    [SerializeField] private TextMeshProUGUI seachResultCountTxt;
+    [SerializeField] private Transform searchedBookContainer;
+
+    private int searchPageIndex = 0;
+    private int filterCategoryID = -1;
+    private int searchedBookCount = 0;
+
+
+    public void NextOrPrevPage(NextOrPrev nextOrPrev)
+    {
+        if (nextOrPrev == NextOrPrev.Next)
+        {
+            //Cache.Instance.search()
+        }
+        else
+        {
+
+        }
+    }
+
+    private void ManageSeachUIForSearchedResult()
+    {
+        if (searchedBookCount == 0)
+        {
+            prevSearchPageBtn.interactable = false;
+            nextSearchPageBtn.interactable = false;
+        }
+        else if (searchedBookCount > 0)
+        {
+            if (searchPageIndex * searchedBookContainer.childCount <= searchedBookCount)
+            {
+                prevSearchPageBtn.interactable = false;
+                nextSearchPageBtn.interactable = false;
+            }
+            else if (searchPageIndex > 2 && searchPageIndex < s)
+            {
+
+            }
+        }
+    }
+    #endregion
+}
+
+public enum NextOrPrev
+{
+    Next,
+    Previous
 }
