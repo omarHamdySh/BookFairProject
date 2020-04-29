449452dcfbce13c806d9a6ba273a9694b07adad0
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2020-04-29 14:22:16 +0200

    bendary - finish splash with the new ui and make new endless loading bar slider

---
+++ Assets/_AppAssets/Scripts/GUI/EndlessSliderLoadingBar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class EndlessSliderLoadingBar : MonoBehaviour
+{
+    [SerializeField] private Vector2[] waypoints;
+    [SerializeField] private float delay;
+
+    [SerializeField] private RectTransform progress;
+
+    private int index = 0;
+    private bool isArraived;
+
+    private void Start()
+    {
+        progress.anchoredPosition = waypoints[index];
+        StartCoroutine(MoveBetweenWayPoints());
+    }
+
+    IEnumerator MoveBetweenWayPoints()
+    {
+        while (true)
+        {
+            isArraived = false;
+            index = (index + 1) % waypoints.Length;
+            progress.DOAnchorPos(waypoints[index], delay).OnComplete(Arrived);
+            yield return new WaitUntil(() => isArraived == true);
+        }
+    }
+
+    private void Arrived()
+    {
+        isArraived = true;
+    }
+}
