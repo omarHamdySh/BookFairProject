3a2f9711f4d62e1b3ff7064ac7b1255f5d32aeec
Nouran  Ayman  Mohamed <nouraaan.aymaaan@gmail.com>
2020-08-17 20:19:14 -0700

    Nouran_ScrollableUISwipeMode

---
+++ Assets/_AppAssets/Scripts/GUI/ScrollableMenu_Nouran/ButtonEventTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonEventTrigger : MonoBehaviour
+{
+    private string _buttonName;
+    private Text _buttonTitle;
+
+    public Vector3 TargetButtonScale;
+
+    public void Start()
+    {
+        _buttonName  = gameObject.name;
+        _buttonTitle = gameObject.transform.GetChild(1).GetComponent<Text>();
+    }
+
+    public void OnButtonHovered()
+    {
+        //Debug.Log(_buttonName + " is hoverd !");
+        _buttonTitle.enabled = true;
+        transform.localScale = new Vector3(TargetButtonScale.x, TargetButtonScale.y, TargetButtonScale.z);
+    }
+
+    public void OnButtonNotHovered()
+    {
+        //Debug.Log(_buttonName + " is not hovered anymore !");
+        _buttonTitle.enabled = false;
+        transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+
+}
