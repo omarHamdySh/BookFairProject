78ef6a45119944074601fc1fbe1f8db1388bf99b
Omar <Omar.h.shafeek@gmail.com>
2020-08-21 18:16:46 +0200

    Omar -  Fixing statistic component

--- Assets/_AppAssets/Scripts/GUI/StatisticsUIHandller.cs
+++ Assets/_AppAssets/Scripts/GUI/StatisticsUIHandller.cs
@@ -41,7 +41,7 @@ public class StatisticsUIHandller : MonoBehaviour
         //GenerateRandom();
 
 
-        StartCoroutine(ShowStatistics());
+        //StartCoroutine(ShowStatistics());
 
     }
 
