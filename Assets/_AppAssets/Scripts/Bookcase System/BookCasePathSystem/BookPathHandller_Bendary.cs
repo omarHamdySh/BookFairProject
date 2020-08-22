518a4dd8c224a6c79ab45a4d4cef344d66f127d5
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2020-08-22 15:18:06 +0200

    Update BookPathHandller_Bendary.cs

--- Assets/_AppAssets/Scripts/Bookcase System/BookCasePathSystem/BookPathHandller_Bendary.cs
+++ Assets/_AppAssets/Scripts/Bookcase System/BookCasePathSystem/BookPathHandller_Bendary.cs
@@ -350,32 +350,5 @@ public class BookPathHandller_Bendary : MonoBehaviour
         return -1;
     }
 
-
-    public int mapBooksIndicies(int index)
-    {
-        switch (index)
-        {
-            case 0:
-                return 4;
-            case 1:
-                return 3;
-            case 2:
-                return 5;
-            case 3:
-                return 2;
-            case 4:
-                return 6;
-            case 5:
-                return 1;
-            case 6:
-                return 7;
-            case 7:
-                return 0;
-            case 8:
-                return 8;
-            default:
-                return 4;
-        }
-    }
     #endregion
 }
