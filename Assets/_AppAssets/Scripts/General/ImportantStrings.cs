21b152af70d1a676aa216ddcb483eea73692ee05
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2020-09-04 15:40:42 +0200

    Revert "Nouran - MainMenuTutorial"
    This reverts commit 69ed069fde756665bd4f3a3321df8ff0a96d4dc4.

--- Assets/_AppAssets/Scripts/General/ImportantStrings.cs
+++ Assets/_AppAssets/Scripts/General/ImportantStrings.cs
@@ -9,6 +9,5 @@ public static class ImportantStrings
     public const string langPPKey = "Lang";
     public const string fairIDKey = "fairID";
     public const string splashScene = "SplashScene_Bendary";
-    //public const string gameplayScene = "GamePlayScene_Bendary";
-    public const string gameplayScene = "GamePlayScene_Nouran";
+    public const string gameplayScene = "GamePlayScene_Bendary";
 }
