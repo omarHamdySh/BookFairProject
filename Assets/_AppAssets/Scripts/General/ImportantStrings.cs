69ed069fde756665bd4f3a3321df8ff0a96d4dc4
Nouran  Ayman  Mohamed <nouraaan.aymaaan@gmail.com>
2020-09-04 15:27:01 +0200

    Nouran - MainMenuTutorial

--- Assets/_AppAssets/Scripts/General/ImportantStrings.cs
+++ Assets/_AppAssets/Scripts/General/ImportantStrings.cs
@@ -9,5 +9,6 @@ public static class ImportantStrings
     public const string langPPKey = "Lang";
     public const string fairIDKey = "fairID";
     public const string splashScene = "SplashScene_Bendary";
-    public const string gameplayScene = "GamePlayScene_Bendary";
+    //public const string gameplayScene = "GamePlayScene_Bendary";
+    public const string gameplayScene = "GamePlayScene_Nouran";
 }
